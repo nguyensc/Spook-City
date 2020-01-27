465aebefdca4602c723c8b42a0c957c85005207f
colliram <colliram@mail.gvsu.edu>
2020-01-27 10:13:30 -0500

    updated assets

---
+++ assets/map assets/TileA4_PHC_Interior-Hospital.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TileA4_PHC_Interior-Hospital" tilewidth="16" tileheight="16" tilecount="368" columns="23">
+ <image source="sprite_sheets/Hospital Tiles/TileA4_PHC_Interior-Hospital.png" width="368" height="256"/>
+</tileset>
---
+++ assets/map assets/TileA5_PHC_Interior-Hospital.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TileA5_PHC_Interior-Hospital" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+ <image source="sprite_sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png" width="128" height="256"/>
+</tileset>
