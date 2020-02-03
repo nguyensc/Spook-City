1e2a7af706f6c63f6850a49485921383cb6b6ec7
colliram <colliram@mail.gvsu.edu>
2020-02-03 10:30:45 -0500

    added mapRenderer

--- src/mapRenderer.py
+++ src/mapRenderer.py
@@ -1,5 +1,6 @@
 import league
 import pygame
+from tmxloader import TiledRenderer
 
 
 class MapParams():
@@ -33,37 +34,26 @@ class MapRenderer():
 
         currMap = mapDict.get(self.mapName)
 
-        switcher = {
-            1: MapParams(
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 8, '../assets/map assets/level 1/floors.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA4_PHC_Interior-Hospital.png', 23, '../assets/map assets/level 1/walls.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA4_PHC_Interior-Hospital.png', 23, '../assets/map assets/level 1/walls.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA4_PHC_Interior-Hospital.png', 23, '../assets/map assets/level 1/walls.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA4_PHC_Interior-Hospital.png', 23, '../assets/map assets/level 1/walls.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA4_PHC_Interior-Hospital.png', 23, '../assets/map assets/level 1/walls.lvl'
+        selector = mapDict[currMap]
 
-            ),
-            2: MapParams(
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 8, '../assets/map assets/level 1/floors.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 8, '../assets/map assets/level 1/floors.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 8, '../assets/map assets/level 1/floors.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 8, '../assets/map assets/level 1/floors.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 8, '../assets/map assets/level 1/floors.lvl',
-                '../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 8, '../assets/map assets/level 1/floors.lvl'
-            )
+        if (selector == 1) {
+            e.
         }
+        else if (selector == 2) {
 
-        selectedMap = switcher.get(currMap)
+        }
+        else if (selector == 3) {
+            
+        }
+        else if (selector == 4) {
+            
+        }
+        else if (selector == 5) {
+            
+        }
+        else if (selector == 6) {
+            
+        }
+        else {
 
-        for i in range(0,5):
-            pos = i * 3
-            sprites = league.Spritesheet(selectedMap[pos], 16, selectedMap[pos + 1])
-            layer = league.Tilemap(selectedMap[pos + 2], sprites, layer = i)
-            self.engine.drawables.add(layer.passable.sprites())
-            self.player.blocks.add(layer.impassable)
-            
-            if (i == 0):
-                worldSize = (layer.wide * league.Settings.tile_size, layer.high * league.Settings.tile_size)
-                self.player.worldSize = worldSize
-                self.player.image.get._rect()
-            
+        }
