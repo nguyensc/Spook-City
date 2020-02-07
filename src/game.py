5d9c35bb44bfe8676e79c5140d19db58ff53d4c6
Scott Nguyen <ngn.scott@gmail.com>
2020-02-07 15:29:19 -0500

    Fixes for merge from lighting

--- src/game.py
+++ src/game.py
@@ -14,26 +14,6 @@ def main() :
     e.init_pygame()
     timer = pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // league.Settings.gameTimeFactor)
     count = 0
-<<<<<<< HEAD
-    floor = league.Spritesheet('../assets/map assets/sprite sheets/Hospital Tiles/TileA5_PHC_Interior-Hospital.png', 16, 8)
-    walls = league.Spritesheet('../assets/map assets/sprite sheets/Hospital Tiles/TileA4_PHC_Interior-Hospital.png', 16, 23)
-    floorLayer = league.Tilemap('../assets/map assets/level 1/floors.lvl', floor, layer = 0)
-    wallLayer = league.Tilemap('../assets/map assets/level 1/walls.lvl', walls, layer = 1)
-    b = league.Tilemap('../assets/map assets/level 1/background.lvl', floor, layer = 0)
-    world_size = (floorLayer.wide*league.Settings.tile_size, floorLayer.high *league.Settings.tile_size)
-    e.drawables.add(floorLayer.passable.sprites())
-    e.drawables.add(wallLayer.passable.sprites())
-    e.drawables.add(b.passable.sprites()) 
-
-    p = Player(1, 400, 300)
-    l = Light(1, 0, 0, p)
-    p.blocks.add(wallLayer.impassable)
-    f = Flashlight(200, 500, 2, p)
-    q = Player(2, 300, 400)
-    o = Overlay(p)
-    f.blocks.add(wallLayer.impassable)
-    p.world_size = world_size
-=======
     p = Player(1, 400, 300)
     map = MapRenderer("first floor", e)
     world_size = map.renderBackground()
@@ -42,7 +22,6 @@ def main() :
     f = Flashlight(200, 500, 2, p)
     q = Player(2, 300, 400)
     o = Overlay(p)
->>>>>>> ba50b5c6f9e171556b67bdf457dcf42071ff2fe6
     p.rect = p.image.get_rect()
     q.image = p.image
     e.objects.append(p)
@@ -54,22 +33,11 @@ def main() :
     e.drawables.add(p)
     e.drawables.add(q)
     e.drawables.add(l)
-<<<<<<< HEAD
-    e.drawables.add(f)    
-
-    c = league.LessDumbCamera(league.Settings.height, league.Settings.width, p, e.drawables, world_size)
-=======
     e.drawables.add(f)
     map.renderForeGround()
     c = league.LessDumbCamera(720, 720, p, e.drawables, world_size)
->>>>>>> ba50b5c6f9e171556b67bdf457dcf42071ff2fe6
     e.objects.append(c)
     e.light_source = l
-<<<<<<< HEAD
-    e.flashlight = f  
-
-=======
->>>>>>> ba50b5c6f9e171556b67bdf457dcf42071ff2fe6
     e.collisions[p] = (q, p.ouch)
   
     e.nospriteables.append((f, (255, 0, 0, 255)))
